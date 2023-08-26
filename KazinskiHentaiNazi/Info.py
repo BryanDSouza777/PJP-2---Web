0fd250c1f7856ddea2019c95075d438aab92a570
BryanD.Souza <bryandsouza777@gmail.com>
2023-08-26 02:01:03 -0300

    Initial commit

---
+++ KazinskiHentaiNazi/Info.py
@@ -0,0 +1,30 @@
+'''
+Vender roupas
+Cores set: preto, amarelo detalhe
+adulto/adolecente
+faixa de preço: 100+
+pagamento: cartão, boleto
+nome: ???
+Fonte: moderna, seriedade
+Frete gratis: RS 200+
+Mensagem: Classe media, estiloso
+Empresa Moderna e Acessivel
+Diferencial: roupa social, m&f
+Sem devolução, só se tiver defeito
+Atentimento, contratar funcionario
+Entrar site, principais peças, promoçoes
+Sessões abas de gravatas, camisa, calça, terno...
+comprar precisa de login
+cupons de canais
+Aba Pesquisa
+cupom desconto
+não ter propaganda exesiva
+Fotos produto, 3/4 posições
+contato: email
+tamanhos p ao gg, maior por encomenda +caro
+não gosto, dinheiro preso, credito na loja porem devolver a conta pessoal
+garantia de lavagem do manual, e costura solta
+media entrega: depende da onde mora
+sede sc
+transportadora
+'''
